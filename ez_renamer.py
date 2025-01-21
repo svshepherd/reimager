2d41b0c4faac3350732c0e43396a8c04dbb042cb
svshepherd <47264767+svshepherd@users.noreply.github.com>
2025-01-21 06:55:39 -0500

    update from paradox

---
+++ ez_renamer.py
@@ -0,0 +1,85 @@
+"""gui for prefixing filenames with the 8-digit date taken."""
+
+import os
+import sys
+#import shutil
+#import requests
+import datetime
+from PyQt5.QtWidgets import QApplication, QFileDialog, QLabel, QVBoxLayout, QWidget
+from PyQt5.QtCore import Qt, QUrl
+from PIL import Image
+from PIL.ExifTags import TAGS, GPSTAGS
+
+
+## now I'd like to learn when the photos were taken and prefix that date (if necessary) 
+## in my standard format
+def extract_date_taken(file):
+    """this looks like it's doing a better job than my original method 
+    for extracting date_taken, independent of subsequent edits."""
+    image = Image.open(file)
+
+    # Extract EXIF data
+    exif_data = image._getexif()
+    if exif_data is not None:
+        for tag, value in exif_data.items():
+            tag_name = TAGS.get(tag, tag)
+            if tag_name == "DateTimeOriginal":
+                # Convert the date format from string to datetime
+                date_time_obj = datetime.datetime.strptime(value, '%Y:%m:%d %H:%M:%S')
+                return date_time_obj
+    return None
+
+
+class DropWidget(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setAcceptDrops(True)
+        self.label = QLabel('Drag and Drop Files Here', self)
+        self.label.setAlignment(Qt.AlignCenter)
+        layout = QVBoxLayout()
+        layout.addWidget(self.label)
+        self.setLayout(layout)
+        self.resize(256, 256)
+
+    def dragEnterEvent(self, event):
+        if event.mimeData().hasUrls():
+            event.accept()
+        else:
+            event.ignore()
+
+    def dropEvent(self, event):
+        image_files = [str(QUrl.toLocalFile(url)) for url in event.mimeData().urls()]
+
+        ## Now I'd like to execute (and report) file name changes:
+        for file in image_files:
+            # split_name = file.split(os.sep) ### curious this doesn't work!
+            split_name = file.split('/')
+            path_name = os.sep.join(split_name[:-1])
+            file_name = split_name[-1]
+            try:
+                day = extract_date_taken(file)
+                day_prefix = f"{day:%Y%m%d}"
+                if file_name.endswith('.ARW'):
+                    continue
+                elif file_name.startswith(day_prefix):
+                    print(file_name, "===== ALREADY GOOD!!")
+                    continue
+                else:
+                    new_name = day_prefix+" "+file_name
+                    os.rename(os.path.join(path_name, file_name), os.path.join(path_name, new_name))
+                    print(file_name, "---->", new_name, "SUCCESSFUL")
+            except Exception as e:
+                print(file, "----X", e)   
+
+
+def main():
+    app = QApplication(sys.argv)
+    widget = DropWidget()
+    widget.show()
+    app.exec_()
+
+if __name__ == '__main__':
+    main()
--- fix_phoneCam_dateTaken.py
+++ fix_phoneCam_dateTaken.py
@@ -1,14 +1,6 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Jul 24 16:45:55 2019
-
-@author: svs
-"""
-
-'''the purpose of this program is to fix the damned stupid fact that my camera 
-images, saved via Seek rather than the camera roll, keep geotagging but 
-mysteriously lose timestamps! note that *if* this works, it only works
-for very basic Exif data'''
+"""the purpose of this program is to fix the damned stupid fact that my camera images, 
+saved via Seek rather than the camera roll, keep geotagging but mysteriously lose 
+timestamps! note that *if* this works, it only works for very basic Exif data"""
 
 import piexif
 
@@ -16,20 +8,22 @@ import os
 from shutil import move
 #from pprint import pprint
 
-os.chdir(r'C:\Users\svs\Desktop\ims\fix_ts')
+if __name__ == '__main__':
 
-for i in [i for i in os.listdir() if i[-4:]=='.jpg'] :
-    if (i[4],i[7]) != ('-','-') :
-        pprint('no')
-        continue
-    #pprint( 'move( ' + i + ', (' + i[0:4] + i[5:7] + i[8:] + ') )' )
-    filename = i
-    exif_dict = piexif.load(filename)
-    exif_dict["Exif"][36867] = \
-        filename[:-4].replace("-",":").replace(".",":")
-        # NOTE: "The character string length is 20 bytes including NULL 
-        # for termination." - https://www.awaresystems.be/imaging/tiff/tifftags/privateifd/exif/datetimeoriginal.html
-        # ...BUT seems to be okay
-    exif_bytes = piexif.dump(exif_dict)
-    piexif.insert(exif_bytes, filename)
-    move( i, (i[0:4]+i[5:7]+i[8:]) )
+    os.chdir(r'C:\Users\svs\Desktop\ims\fix_ts')
+
+    for i in [i for i in os.listdir() if i[-4:]=='.jpg'] :
+        if (i[4],i[7]) != ('-','-') :
+            pprint('no')
+            continue
+        #pprint( 'move( ' + i + ', (' + i[0:4] + i[5:7] + i[8:] + ') )' )
+        filename = i
+        exif_dict = piexif.load(filename)
+        exif_dict["Exif"][36867] = \
+            filename[:-4].replace("-",":").replace(".",":")
+            # NOTE: "The character string length is 20 bytes including NULL 
+            # for termination." - https://www.awaresystems.be/imaging/tiff/tifftags/privateifd/exif/datetimeoriginal.html
+            # ...BUT seems to be okay
+        exif_bytes = piexif.dump(exif_dict)
+        piexif.insert(exif_bytes, filename)
+        move( i, (i[0:4]+i[5:7]+i[8:]) )
